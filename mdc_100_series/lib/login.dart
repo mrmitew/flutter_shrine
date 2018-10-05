6d7cd0ce9440669428401e995fa4605e1a4c6f56
Stefan Mitev <mr.mitew@gmail.com>
2018-10-05 10:52:40 +0200

    [add] login user/pass textfield widgets

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -40,8 +40,21 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
             // TODO: Add button bar (101)
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true
+            ),
           ],
         ),
       ),
